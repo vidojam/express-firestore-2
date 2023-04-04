1238eb2acd2c538a898b001763115e44012569c9
Jose Torres <vidojam@gmail.com>
2023-04-04 16:10:32 -0400

    add endpoint and imported tvLibrary

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 
+import { addNewTv, getAllTv} from './src/tvLibray.js';
+
 const PORT = 3000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/')
-
+app.get('/tvShows', getAllTv);
+app.post('/tvShows', addNewTv);
 
 
 app.listen(PORT, () => {
